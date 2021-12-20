25ed4f8de1b1c8763131cb958535c0818bbc29aa
voilalex <ilya.vouk@gmail.com>
2021-12-21 00:08:44 +0300

    Fix project name

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except Exception:
 
 setup(
 	# Name of the package 
-	name='requirements-txt',
+	name='to-requirements.txt',
 	# Packages to include into the distribution 
 	packages=find_packages('.'),
 	# Start with a small number and increase it with 
